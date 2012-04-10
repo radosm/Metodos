2955021a89118509ece5f36bef97456a63877de9
Martin Rados <radosm@gmail.com>
2012-04-10 11:01:26 -0300

    TP1

--- Abril_8/ejecucion_biseccion.sh
+++
@@ -1,9 +0,0 @@
-typeset -i i
-i=1
-while [ $i -lt 53 ]
-do
-#             precision  tolerancia  max_iter  altura  velocidad  masa  cr  fr
-#             ---------  ----------  --------  ------  ---------  ----  --  --
-  ./biseccion $i         0.0001      10000     0       10         2     0   1
-  i=$i+1
-done
