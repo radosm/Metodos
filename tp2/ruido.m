64741e12801bf3d2b537be783775313c41f42ff5
Martin Rados <radosm@gmail.com>
2012-05-19 22:00:39 -0300

    patriada

--- tp2/ruido.m
+++ tp2/ruido.m
@@ -1,6 +1,7 @@
 %I=imread("64x64/blond.pgm");
 I=imread("128x128/einstein.pgm");
-J=imnoise(im2double(I),'gaussian',0,0.01);
+J=imnoise(im2double(I),'speckle',0.04);
+%J=imnoise(im2double(I),'gaussian',0,0.01);
 %J=imnoise(im2double(I),'salt & pepper');
 J=im2uint8(J);
 imshow(J);
