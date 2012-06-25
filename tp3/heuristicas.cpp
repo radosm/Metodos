a01afedb13a765eff7481a438883c6fe34b7e914
Martin Rados <radosm@gmail.com>
2012-06-25 17:01:04 -0300

    h1

--- tp3/heuristicas.cpp
+++ tp3/heuristicas.cpp
@@ -11,44 +11,40 @@ void h1(int n,Coef m0, Coef ml, Coef mp, vector<Coef> &k, vector<int> &l, vector
 {
   SetPruebas m;
   vector<Coef> v(n);
-  for(int c=1;c < 11;c++){
+  for(int c=1;c < 51;c++){
     int cl=0;
     int cp=c;
     while (m.size()<=c*n){
       for (int i=0;i<=cl;i++){
-        for (int j=cp;i>=0;j--){
+        for (int j=cp;j>=0;j--){
           for (int x=0;x<=i;x++){
             int ps=0;
 	    int pp=0;
-            while (ps==pp || l[ps]==0) { ps=rand()%n; int pp=rand()%n; }
-            cout << " x=" << x
-                 << " ps=" << ps
-                 << " pp=" << pp
-                 << endl;
+            while (ps==pp || l[ps]==0) { ps=rand()%n; pp=rand()%n; }
             l[ps]--;
             l[pp]++;
 	  }
           for (int x=0;x<=j;x++){
             int ps=0;
 	    int pp=0;
-            while (ps==pp || p[ps]==0) { ps=rand()%n; int pp=rand()%n; }
-            cout << " x=" << x
-                 << " ps=" << ps
-                 << " pp=" << pp
-                 << endl;
+            while (ps==pp || p[ps]==0) { ps=rand()%n; pp=rand()%n; }
+            //cout << " x=" << x
+                 //<< " ps=" << ps
+                 //<< " pp=" << pp
+                 //<< endl;
             p[ps]--;
             p[pp]++;
 	  }
         }
       }
       for (int i=0;i<n;i++){
-        v[i]=1/(m0+ml*l[i]+mp*p[i]);
+        v[i]=(m0+ml*l[i]+mp*p[i]);
       }
       m.insert(v);
-      cout <<  "cl=" << cl
-           << " cp=" << cp
-           << " m.size()=" << m.size()
-           << endl;
+      //cout <<  "cl=" << cl
+           //<< " cp=" << cp
+           //<< " m.size()=" << m.size()
+           //<< endl;
     }
     P.push_back(m);
   }
