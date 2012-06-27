1f2bbceda0ef47766081913343d9e59e4329825b
Martin Rados <radosm@gmail.com>
2012-06-27 09:58:08 -0300

    mas esqueletos

--- tp3/heuristicas.cpp
+++ tp3/heuristicas.cpp
@@ -57,13 +57,6 @@ void h1(int n,Coef m0, Coef ml, Coef mp, Matriz& K, vector<int> l, vector<int> p
     if (l[ps]!=0) cl=rand()%(l[ps]); // Cuantos livianos saco
     if (p[ps]!=0) cp=rand()%(p[ps]); // Cuantos pesados saco
 
-//cout << i << " " 
-//<< ps << " " 
-//<< pp << " " 
-//<< cl << " " 
-//<< cp << " " 
-//<< endl;
-
     l[ps]-=cl;
     l[pp]+=cl;
     p[ps]-=cp;
@@ -108,7 +101,7 @@ void h1(int n,Coef m0, Coef ml, Coef mp, Matriz& K, vector<int> l, vector<int> p
     if (ok) {
       cout << endl;
       cout << "Solución encontrada:" << endl;
-      for (int i=0;i<n;i++) { cout << "w[" << i << "]=" << w[i] << endl; }
+      for (int i=0;i<n;i++) { cout << "  w[" << i << "]=" << w[i] << endl; }
       break;
     }
     
@@ -127,4 +120,50 @@ void h1(int n,Coef m0, Coef ml, Coef mp, Matriz& K, vector<int> l, vector<int> p
 }
 
 void h2(int n,Coef m0, Coef ml, Coef mp, Matriz& K, vector<int> l, vector<int> p, bool& ok, bool& tmax, vector< Piso >& P){
+  
+  P.resize(n);
+  for (int i=0;i<n;i++){ // La primera prueba es como vino originalmente
+    P[i].l=l[i];
+    P[i].p=p[i];
+  }
+
+  vector<Coef> w(n); // Acá quedarán las frecuencias
+  clock_t inicio,fin;
+  double segundos;
+  Matriz Qac;
+  Matriz Dant;
+
+  inicio=clock();
+  int c=0;
+  while (true){
+    if (!(c%10)) cout << "." << flush; // Para ver que está trabajando
+
+    //
+    // Aca va la heuristica
+    //
+
+    // Calcula el tiempo transcurrido
+    fin=clock();
+    segundos=(double)(fin - inicio)/CLOCKS_PER_SEC;
+
+    // Si las frecuencias no están en el rango prohibido terminamos=>sale
+    if (ok) {
+      cout << endl;
+      cout << "Solución encontrada:" << endl;
+      for (int i=0;i<n;i++) { cout << "  w[" << i << "]=" << w[i] << endl; }
+      break;
+    }
+    
+    if (segundos > 60) { // Máximo tiempo permitido para las pruebas
+      cout << endl;
+      cout << "Tiempo máximo alcanzado!" << endl;
+      tmax=true;
+      break;
+    }
+    c++;
+  }
+  if (!ok && !tmax) cout << endl;
+
+  cout << "Cantidad de pruebas: " << c << "." << endl;
+  cout << "Tiempo ejecucion: " << setprecision(15) << segundos << " segundos." << endl;
 }
